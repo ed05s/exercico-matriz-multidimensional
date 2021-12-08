ca0c0421d1a7bebc44c76a393d391a0f5f59bc99
ed05s <edinaldolouredomarques@gmail.com>
2021-12-07 21:23:21 -0300

    primeiro commit

---
+++ Exe4_Matriz.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe4_matriz;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Exe4_Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random random =new Random();
+        int[][] M = new int[4][4];
+        
+        for(int i = 0; i< M.length;i++){
+            for (int j=0 ;j<M[i].length;j++){
+                M[i][j]= random.nextInt(9);
+                
+            }  
+        }
+        System.out.println("Matriz: ");
+        for(int[] linha : M){
+            for(int coluna : linha){
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# exercico-matriz-multidimensional
